1a69c3565f02c937877b2419f0881871852e0a55
bruski <bruski@qq.com>
2020-01-28 22:52:27 +0800

    使用url-loader打包图片

--- loader/webpack.config.js
+++ loader/webpack.config.js
@@ -12,13 +12,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jpg$/,
+        // 图片文件的打包配置
+        test: /\.(png|jpe?g|gif)$/i,
         use: {
-          loader: 'file-loader', // 文件处理loader
+          loader: 'url-loader', // 文件处理loader
           options: {
             // placeholder 占位符
             name: '[name]_[hash].[ext]', // 导出的名字：原名+原扩展名
-            outputPath: 'images/'
+            outputPath: 'images/',
+            limit: 20480 // 如果小于20kb(2048字节)，以base64放到js中；否则以图片文件打包到目录中。
           }
         }
       }
